65a59eaa929655f59eef2ddf47101d7ee0638a28
Raeshana <rsookhoo@andrew.cmu.edu>
2025-04-08 18:07:50 -0400

    Update Button Prefab

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [ContextMenu("Transition to next scene")]
+    public void GoToNextScene() 
+    {
+        StartCoroutine(GoToNextSceneRoutine());
+    }
+
+    private IEnumerator GoToNextSceneRoutine()
+    {
+        int current = SceneManager.GetActiveScene().buildIndex;
+        int next = current + 1;
+
+        yield return new WaitForSeconds(0.35f);
+
+        SceneManager.LoadScene(next, LoadSceneMode.Single);
+    }
+
     [ContextMenu("Go to act 1 info scene")]
     public void GoToAct1InfoScene()
     {
